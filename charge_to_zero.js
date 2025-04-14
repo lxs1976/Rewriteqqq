7ef158d4785c1fa967fcf8f3c3a333d9c822b3e3
lxs1976 <39977911+lxs1976@users.noreply.github.com>
2025-04-14 12:36:48 +0800

    Update charge_to_zero.js

--- charge_to_zero.js
+++ charge_to_zero.js
@@ -6,18 +6,15 @@
 hostname = edu.51itzl.com
 // charge_to_zero.js
 var bodyData = JSON.parse($response.body);
+var requestBody = JSON.parse($request.body);
 
-// 如果是数组，遍历每个对象并设置 charge=0
-if (Array.isArray(bodyData)) {
-    bodyData.forEach(item => {
-        if (item && typeof item === 'object') {
-            item.charge = 0;
-        }
-    });
-} 
-// 如果是单个对象，直接设置 charge=0
-else if (bodyData && typeof bodyData === 'object') {
-    bodyData.charge = 0;
+// 只有当请求体包含 type=JavaScript 时才修改
+if (requestBody.type === "JavaScript") {
+    if (Array.isArray(bodyData)) {
+        bodyData.forEach(item => { item.charge = 0; });
+    } else if (bodyData && typeof bodyData === 'object') {
+        bodyData.charge = 0;
+    }
 }
 
 $done({body: JSON.stringify(bodyData)});
